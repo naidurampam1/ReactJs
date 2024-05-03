05a53cba022ecf3a4866c3a0190bca83138fe6ce
Naidurampam <naidurampam5555@gmail.com>
2024-05-03 04:00:34 +0000

    React_congrats_card

---
+++ ReactJs/index.js
@@ -0,0 +1,20 @@
+const element = (
+  //   Write your code here.
+  <div>
+    <div className="backgroundColor">
+      <h1 className="headingClr">Super Over league</h1>
+      <div class="centerImg">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+          className="imgSize"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+          className="imgSize"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
